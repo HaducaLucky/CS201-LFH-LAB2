eb727df0e22afb19354cff8c7406bc70b677b209
linkx16 <147017329+linkx16@users.noreply.github.com>
2023-10-06 10:59:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstFavNum = 16;
+let secondFavNum = 6;
+
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum != secondFavNum);
